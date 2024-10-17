b35b1d7ea3a7eae105a61db126910fb15df2a7c3
kirik-streltsov <kirik.streltsov0@yandex.ru>
2024-10-17 14:56:58 +0300

    Hope it will work :(

--- scripts/active.js
+++ scripts/active.js
@@ -4,16 +4,18 @@ const currFile = currLocationItems[currLocationItems.length - 1]
 
 const menuItems = document.getElementsByClassName('menu_item')
 
+if (document.location.href == 'https://kirik-streltsov.github.io/frontend-labs/') {
+    let main = document.getElementById('main')
+    main.classList.remove('menu_item')
+    main.classList.add('menu_item_active')
+    return
+}
+
 for (let item of menuItems) {
     console.log(item.className)
     if (item.getAttribute('href') == currFile) {
         item.classList.remove('menu_item')
         item.classList.add('menu_item_active')
-        break
-    } else if (currFile == 'https://kirik-streltsov.github.io/frontend-labs/') {
-        let main = document.getElementById('main')
-        main.classList.remove('menu_item')
-        main.classList.add('menu_item_active')
         break
     }
 }
