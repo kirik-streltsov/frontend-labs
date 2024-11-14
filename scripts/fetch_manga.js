8796809f5268caccf2b2ca2236bdcb976f63a9ef
kirik-streltsov <kirik.streltsov0@yandex.ru>
2024-11-14 15:40:32 +0300

    Done edits

--- scripts/fetch_manga.js
+++ scripts/fetch_manga.js
@@ -1,29 +1,23 @@
 let generateView = (link, imgSrc, mangaName) => {
-    let template = 
-    `
-    <div>
-        <a href="${link}" class="manga-link">
-            <img src="${imgSrc}" alt="Manga">
-            <div class="span-container">
-                <span>${mangaName}</span>
-            </div>
-        </a>
-    </div>
-    `
+    let template = document.getElementById('manga-template');
+    let element = document.createElement('div');
 
-    let element = document.createElement('div')
-    element.innerHTML = template
+    element.append(template.content.cloneNode(true));
+    element.querySelector('a').setAttribute('href', link);
+    element.querySelector('img').setAttribute('src', imgSrc);
+    element.querySelector('span').innerText = mangaName;
 
-    return element
-}
+    return element;
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-    const mangaGrid = document.getElementById('manga-grid')
-    const loader = document.querySelector(".loader")
-    fetch("https://jsonplaceholder.typicode.com/photos")
+    const mangaGrid = document.getElementById('manga-grid');
+    const loader = document.querySelector(".loader");
+    let albumId = Math.floor(Math.random() * (10 - 1) + 1);
+    fetch("https://jsonplaceholder.typicode.com/photos?albumId=" + albumId)
         .then(response => response.json())
         .catch(err => {
-            loader.remove()
+            loader.remove();
             Toastify({
                 text: "Could not find any manga :(",
                 duration: 3000,
@@ -31,12 +25,12 @@ document.addEventListener('DOMContentLoaded', () => {
                     background: "#D80032",
                 },
                 gravity: "bottom",
-            }).showToast()
-            console.log(err)
+            }).showToast();
+            console.log(err);
         })
-        .then(json => json.slice(1, 17).forEach(element => {
-            let view = generateView(element["url"], element["thumbnailUrl"], element["title"])
-            mangaGrid.appendChild(view)
-            loader.remove()
+        .then(json => json.forEach(element => {
+            let view = generateView(element["url"], element["thumbnailUrl"], element["title"]);
+            mangaGrid.appendChild(view);
+            loader.remove();
         }));
 })
